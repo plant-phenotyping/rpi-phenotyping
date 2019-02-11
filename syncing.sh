27845006d4f7361bdc9bfa35a8ad4d1f96a60e08
m0hamad <jamaledd@ualberta.ca>
2019-02-11 15:41:49 -0700

    Made .sh file executable

--- syncing.sh
+++ syncing.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+#Random Comment
 rsync -av -e ssh ~/Desktop/camera1/ pi@142.244.190.228:/root/mnt/PIHDD/zone3/camera1/
 rsync -av -e ssh ~/Desktop/camera2/ pi@142.244.190.228:/root/mnt/PIHDD/zone3/camera2/
