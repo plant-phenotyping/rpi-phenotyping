f1f9419f70136c00e68b77d757fe6793dddf4d42
m0hamad <jamaledd@ualberta.ca>
2019-03-20 15:59:17 -0600

    2019-03-20 15:16:24.916441

--- capture_image.py
+++ capture_image.py
@@ -2,11 +2,12 @@
 
 import RPi.GPIO as gp
 import os
+import socket
 import datetime as dt
 
 gp.setwarnings(False)
-
 gp.setmode(gp.BOARD)
+
 gp.setup(7, gp.OUT)
 gp.setup(11, gp.OUT)
 gp.setup(12, gp.OUT)
@@ -21,41 +22,41 @@ def main():
     gp.output(12, True)
     capture(1)
 
-    gp.output(7, True)
-    gp.output(11, False)
-    gp.output(12, True)
+    gp.output(7, False)
+    gp.output(11, True)
+    gp.output(12, False)
     capture(2)
 
 def get_time_and_date(date_time):
 
     date = date_time.split(" ")[0].split("-")
-    new_date = date[1] + date[2] + date[0]
+    # new_date = date[1] + date[2] + date[0]
     
     # print(date)
     # print(new_date)
     
-    time = date_time.split(" ")[1].split(".")
+    time = date_time.split(" ")[1].split(".")[0].split(":")
     new_time = time[0]
     
     # print(time)
     # print(new_time)
 
-    return new_date, new_time
+    return date, new_time
 
 def capture(cam):
 
     date_time = str(dt.datetime.now())
 
     date, time = get_time_and_date(date_time)
+    zone_number = str(socket.gethostname())[-1]
 
-    # cmd = "raspistill -o z3c1_" + date + "_" + time + ".png"
-    cmd = "raspistill -o ~/Desktop/camera%d/z3c%d_%s_%s.png" % (cam, cam, date, time)
+    cmd = "raspistill -o ~/Desktop/camera%d/z%sc%d_%s_%s.png" % (cam, zone_number, cam, date, time)
     os.system(cmd)
-
+    
 if __name__ == "__main__":
-
+    
     main()
-
+    
     gp.output(7, False)
     gp.output(11, False)
     gp.output(12, True)
