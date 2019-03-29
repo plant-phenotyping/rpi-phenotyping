b79bce8fc5347f31ac01b9a57ad7ccac6a46d40e
mjenius <jamaledd@ualberta.ca>
2019-03-28 22:42:55 -0600

    Python and Shell scripts are done

--- syncing.sh
+++ syncing.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Zone 1
-# rsync -av -e ssh ~/Desktop/camera1/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone1/camera1/
-# rsync -av -e ssh ~/Desktop/camera2/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone1/camera2/
+# rsync --ignore-existing --remove-source-files -av -e ssh ~/Desktop/camera1/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone1/camera1/
+# rsync --ignore-existing --remove-source-files -av -e ssh ~/Desktop/camera2/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone1/camera2/
 
 # Zone 2
-# rsync -av -e ssh ~/Desktop/camera1/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone2/camera1/
-# rsync -av -e ssh ~/Desktop/camera2/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone2/camera2/
+# rsync --ignore-existing --remove-source-files -av -e ssh ~/Desktop/camera1/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone2/camera1/
+# rsync --ignore-existing --remove-source-files -av -e ssh ~/Desktop/camera2/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone2/camera2/
 
 # Zone 3
-# rsync -av -e ssh ~/Desktop/camera1/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone3/camera1/
-# rsync -av -e ssh ~/Desktop/camera2/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone3/camera2/
+# rsync --ignore-existing --remove-source-files -av -e ssh ~/Desktop/camera1/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone3/camera1/
+# rsync --ignore-existing --remove-source-files -av -e ssh ~/Desktop/camera2/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone3/camera2/
 
 # Zone 4
-# rsync -av -e ssh ~/Desktop/camera1/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone4/camera1/
-# rsync -av -e ssh ~/Desktop/camera2/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone4/camera2/
+# rsync --ignore-existing --remove-source-files -av -e ssh ~/Desktop/camera1/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone4/camera1/
+# rsync --ignore-existing --remove-source-files -av -e ssh ~/Desktop/camera2/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone4/camera2/
 
 # Zone 5
-# rsync -av -e ssh ~/Desktop/camera1/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone5/camera1/
-# rsync -av -e ssh ~/Desktop/camera2/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone5/camera2/
+# rsync --ignore-existing --remove-source-files -av -e ssh ~/Desktop/camera1/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone5/camera1/
+# rsync --ignore-existing --remove-source-files -av -e ssh ~/Desktop/camera2/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone5/camera2/
 
 # Zone 6
-# rsync -av -e ssh ~/Desktop/camera1/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone6/camera1/
-# rsync -av -e ssh ~/Desktop/camera2/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone6/camera2/
+# rsync --ignore-existing --remove-source-files -av -e ssh ~/Desktop/camera1/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone6/camera1/
+# rsync --ignore-existing --remove-source-files -av -e ssh ~/Desktop/camera2/* pi@plantomics.ccis.ualberta.ca:/root/mnt/PIHDD/zone6/camera2/
